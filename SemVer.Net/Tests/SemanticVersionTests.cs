5efab45760d8ef67b14e9c6635da320d75936b42
Vadym Moskalenko <major_1987@ukr.net>
2017-08-06 11:16:55 +0200

    add solution with core and test projects

---
+++ SemVer.Net/Tests/SemanticVersionTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Core;
+using Xunit;
+
+namespace Tests
+{
+    public class SemanticVersionTests
+	{
+        [Fact]
+        public void VersionComare()
+        {
+			var version = new SemanticVersion();
+
+		}
+    }
+}
